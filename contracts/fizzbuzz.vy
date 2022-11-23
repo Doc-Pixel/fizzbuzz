97b0d1b238ad6856677ee9fa28fdcb3c8543fe52
data beast <github@databeast.it>
2022-11-22 20:15:29 -0800

    added assertion

--- contracts/fizzbuzz.vy
+++ contracts/fizzbuzz.vy
@@ -3,6 +3,7 @@
 @view
 @external
 def checkFB(i: uint256) -> String[78]:
+    assert i > 0, "enter value greater than zero"
     if i % 3 == 0 and i % 5  == 0:
         return "fizzbuzz"
     elif i % 3 == 0:
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,5 +1,5 @@
 import pytest
-from brownie import accounts, Contract
+from brownie import accounts, Contract, reverts
 
 ##### test fixtures #####
 @pytest.fixture
@@ -13,6 +13,8 @@ def isolation(fn_isolation):
 
 
 def test_checkFB(fbContract):
+    with reverts():
+        assert fbContract.checkFB(0) == 'fizbuzz'
     assert fbContract.checkFB(1) == '1'
     assert fbContract.checkFB(3) ==  'fizz'
     assert fbContract.checkFB(5) ==  'buzz'
